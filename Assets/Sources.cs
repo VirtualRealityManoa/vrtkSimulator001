aae858c5b1f6b231798a708dc8bc08765d5c29e6
Creighton Gorai <creighton-gorai@users.noreply.github.com>
2018-02-01 07:52:11 -1000

    committing small changes

---
+++ Assets/Sources.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sources : MonoBehaviour {
+
+
+
+
+	/* List of sources:
+	 * 
+	 * 
+	 * tiled floor: https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldmosaictile.com%2Fwp-content%2Fuploads%2F2014%2F04%2FPARIS_METRO_7_Charcoal-on-Off-White.jpg&f=1
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+
+
+
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
