2422464f7ba0464757cc1cea9ad3ead1337e9c5d
Brian Mayeshiro <brianmay@hawaii.edu>
2018-03-29 14:40:08 -1000

    added GR Bot scene for testing, created GR Bot script, added GR audio

---
+++ Assets/Scripts/GR_Bot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GR_Bot : MonoBehaviour 
+{
+
+    private AudioSource audioSource;
+    public AudioClip[] GRlines;
+    private AudioClip GRClip;
+
+	// Use this for initialization
+	void Start () 
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int index = Random.Range(0, GRlines.Length);
+            GRClip = GRlines[index];
+            audioSource.clip = GRClip;
+            audioSource.Play();
+        }
+    }
+}
