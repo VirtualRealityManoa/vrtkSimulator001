70b536e78e9492516f00b2bc85c4dacb40c7922f
Brian Mayeshiro <brianmay@hawaii.edu>
2018-04-14 14:51:23 -1000

    Added WalkPath script, automated GRbot line generation

--- Assets/Scripts/GR_Bot.cs
+++ Assets/Scripts/GR_Bot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GR_Bot : MonoBehaviour 
 {
 
+    public float nextPlayTime;
+    public float delay = 2;
     private AudioSource audioSource;
     public AudioClip[] GRlines;
     private AudioClip GRClip;
@@ -13,17 +15,19 @@ public class GR_Bot : MonoBehaviour
 	void Start () 
     {
         audioSource = gameObject.GetComponent<AudioSource>();
+        nextPlayTime = Time.time;
     }
 	
 	// Update is called once per frame
 	void Update () 
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Time.time >= nextPlayTime)
         {
             int index = Random.Range(0, GRlines.Length);
             GRClip = GRlines[index];
             audioSource.clip = GRClip;
             audioSource.Play();
+            nextPlayTime = Time.time + delay;
         }
     }
 }
---
+++ Assets/WalkPath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPath : MonoBehaviour {
+
+    //public Transform startingPoint;
+    public Transform[] destinationPoints;
+    public bool reverse = false;
+    private int index = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+       // startingPoint = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (index == destinationPoints.Length)
+        {
+            reverse = true;
+        }
+        if (transform.position != destinationPoints[index].position && index < destinationPoints.Length)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destinationPoints[index].position, Time.deltaTime * 1);
+ 
+        }
+        else if(reverse == false && index != destinationPoints.Length)
+        {
+            index++;
+        }
+        else
+        {
+            index--;
+        }
+
+	}
+}
